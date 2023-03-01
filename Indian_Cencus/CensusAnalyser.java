960693e27ba92177409b274018a076aa435eecd9
codeaadi <adityadubey362@gmail.com>
2023-03-01 08:31:36 +0530

    Added: Methoid to find indin census

---
+++ Indian_Cencus/CensusAnalyser.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.Indian_Cencus;
+
+public class CensusAnalyser {
+    public static void main(String[] args) {
+        System.out.println("welcome to census analyser");
+    }
+}
